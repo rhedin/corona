5cdab8849fba3e55ccd6bf093bbc6f924815923b
Rick Hedin <cubsno1@gmail.com>
2020-04-07 20:37:38 -0400

    Now both UI elements contain data supplied by the javascript.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,25 @@ export class AppComponent {
   title = 'corona';
 
   countries = [
-    'Canada', 
-    'United States', 
-    'Mexico'
+    {ident: 'canada', name: 'Canada'}, 
+    {ident: 'unitedstates', name: 'United States'}, 
+    {ident:'mexico', name: 'Mexico'}
+  ];
+
+  days = [
+    {date:'02/16/2020', numberOfCases: 15},
+    {date: '02/15/2020', numberOfCases: 10}, 
+    {date: '02/14/2020', numberOfCases: 3}, 
   ];
 
   constructor(private httpClient: HttpClient) {
      
   }
 
-  selectionChanged(event): void {
+  selectionChanged(event: any): void {
     console.log('In selectionChanged.');
     console.log('event passed is', event);
+    console.log('event.value', event.value);
+    console.log('event.target.value', event.target.value);
   }
 }
